f726b466c37e3951572c2470421c66a637efbf4d
corinnebosley <corinne.bosley@metoffice.gov.uk>
2022-09-14 15:52:43 +0100

    all ready and tested except user license, which is PR#9, awaiting review and merge (https://github.com/ADAQ-AQI/cap-sample-data/pull/9)

--- cap-sample-data/__init__.py
+++ cap-sample-data/__init__.py
@@ -2,7 +2,5 @@
 
 import os
 
-from ._version import version as __version__
-
 # Root directory containing the iris sample data.
 path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "sample_data")
