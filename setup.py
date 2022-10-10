7dd9b32d022af18cfb5dd283894c52156c98cf48
corinnebosley <corinne.bosley@metoffice.gov.uk>
2022-10-10 13:30:38 +0100

    added some options to setup.py to try and find data

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    # ...,
+    packages=find_packages(where="cap_sample_data"),
+    package_dir={"": "cap_sample_data"},
+    package_data={"sample_data/*": ["*.nc, *.csv, *.yaml, *.xlsx, *.geojson, "
+                                    "*.zip"]}
+)
+
