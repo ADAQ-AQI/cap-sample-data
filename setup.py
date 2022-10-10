1412c2a2cb6cd64790be0d25079d30594483277f
corinnebosley <corinne.bosley@metoffice.gov.uk>
2022-10-10 14:23:11 +0100

    tried a bit harder with setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@
 from setuptools import setup, find_packages
 
 setup(
-    # ...,
-    packages=find_packages(where="cap_sample_data"),
-    package_dir={"": "sample_data"},
     package_data={"sample_data/*": ["*.nc, *.csv, *.yaml, *.xlsx, *.geojson, "
                                     "*.zip"]}
 )
